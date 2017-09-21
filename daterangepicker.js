afd4d196a1469a4137f39d129a840a2589715d4f
neemanjabu <nemanjabu@gmail.com>
2017-09-21 17:02:12 +0200

    Update daterangepicker.js
    Update datepicker values every time, even if date is not changed.

--- daterangepicker.js
+++ daterangepicker.js
@@ -1136,8 +1136,9 @@
             }
 
             //if a new date range was selected, invoke the user callback function
-            if (!this.startDate.isSame(this.oldStartDate) || !this.endDate.isSame(this.oldEndDate))
-                this.callback(this.startDate, this.endDate, this.chosenLabel);
+            //if (!this.startDate.isSame(this.oldStartDate) || !this.endDate.isSame(this.oldEndDate))
+            //call callback every time, even if old and new dates are the same
+            this.callback(this.startDate, this.endDate, this.chosenLabel);
 
             //if picker is attached to a text input, update it
             this.updateElement();
